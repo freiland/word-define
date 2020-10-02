e9baf7b70b09dde1395ca3dfc2cb25284c499615
Forrest Reiland <you@example.com>
2020-10-02 10:31:06 -0700

    add spec test for save, test pass

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -8,25 +8,33 @@ describe '#Definition' do
   before (:each) do
     Word.clear()
     Definition.clear()
-    @word1 = Word.new("Giants", nil)
-    @word1.save()
+    @word = Word.new("Giants", nil)
+    @word.save()
   end
 
   describe ('#==') do 
     it ('is the same definition if it has the same attributes as another definition') do
-      def1 = Definition.new("big", @word1.id, nil)
-      def2 = Definition.new("big", @word1.id, nil)
+      def1 = Definition.new("big", @word.id, nil)
+      def2 = Definition.new("big", @word.id, nil)
       expect(def1).to(eq(def2))
     end
   end
 
   describe('.all') do 
     it('returns a list of all definitions') do 
-      def5 = Definition.new("test33", @word1.id, nil)
+      def5 = Definition.new("test33", @word.id, nil)
       def5.save()
-      def22 = Definition.new("44test", @word1.id, nil)
+      def22 = Definition.new("44test", @word.id, nil)
       def22.save()
       expect(Definition.all).to(eq([def5, def22]))
+    end
+  end
+
+  describe ('#save') do 
+    it('saves a definition') do 
+      def1 = Definition.new("testttt", @word.id, nil)
+      def1.save()
+      expect(Definition.all).to(eq([def1]))
     end
   end
 end
