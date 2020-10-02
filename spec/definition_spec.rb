c153d728e968460112f86806b03ae93f675df2d4
Forrest Reiland <you@example.com>
2020-10-02 10:18:41 -0700

    #Word # definitions test will not pass, 2 extra definitions are being pulled from a different spec file, have tried to debug, need to move on to finish project

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -8,25 +8,25 @@ describe '#Definition' do
   before (:each) do
     Word.clear()
     Definition.clear()
-    @word = Word.new("Giants", nil)
-    @word.save()
+    @word1 = Word.new("Giants", nil)
+    @word1.save()
   end
 
   describe ('#==') do 
     it ('is the same definition if it has the same attributes as another definition') do
-      def1 = Definition.new("big", @word.id, nil)
-      def2 = Definition.new("big", @word.id, nil)
+      def1 = Definition.new("big", @word1.id, nil)
+      def2 = Definition.new("big", @word1.id, nil)
       expect(def1).to(eq(def2))
     end
   end
 
-  #describe('.all') do 
-    #it('returns a list of all definitions') do 
-      #def1 = Definition.new("big", @word.id, nil)
-      #def1.save()
-      #def2 = Definition.new("small", @word.id, nil)
-      #def2.save()
-      #expect(Definition.all).to(eq([def1, def2]))
-    #end
-  #end
+  describe('.all') do 
+    it('returns a list of all definitions') do 
+      def5 = Definition.new("test33", @word1.id, nil)
+      def5.save()
+      def22 = Definition.new("44test", @word1.id, nil)
+      def22.save()
+      expect(Definition.all).to(eq([def5, def22]))
+    end
+  end
 end
